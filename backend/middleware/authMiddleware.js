d463fae3f2dda4876289f666ba756716cd50d07e
Chandan0Singh <code.chandansingh@gmail.com>
2026-07-02 01:31:34 +0530

    console remove

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -36,16 +36,13 @@ const User = require("../models/User");
 
 const protect = async (req, res, next) => {
   try {
-    console.log("console.log(req.headers);", req.headers);
     const token = req.headers.authorization?.split(" ")[1];
-    console.log("Token from headers:", token); // Debugging line
     if (!token) return res.status(401).json({ message: "Not authorized" });
 
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.user = await User.findById(decoded.id).select("-password");
     if (!req.user) return res.status(401).json({ message: "User not found" });
-    console.log("Authenticated user:", req.user); // Debugging line
-
+    
     next();
   } catch (err) {
     res.status(401).json({ message: "Invalid token" });
